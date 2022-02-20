727e9d462e8fd3d95b9eada53546dbe152ed657a
coolblast-coder <lyapunovantik@gmail.com>
2022-02-20 17:34:53 -0500

    added multer, cloudinary, streamifier to package.json and configed it in server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ const express = require('express');
 const app = express();
 
 const path = require('path');
+
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
+
 const blogService = require('./blog-service')
 
 
@@ -23,6 +29,16 @@ const HTTP_PORT = process.env.PORT || 8080
 const onHttptart = () => console.log('Express http server listening on ' + HTTP_PORT);
 
 app.use(express.static('public'));
+
+
+cloudinary.config({
+    cloud_name: 'alyapunovseneca',
+    api_key: '726415785191287',
+    api_secret: 'ej0Nlo8qDnjYPBmH1tBssOmqdIQ',
+    secure: true
+});
+
+const upload = multer(); // no { storage: storage } since we are not using disk storage
 
 app.get('/', (req, res) => {
     res.redirect('/about');
@@ -60,6 +76,10 @@ app.get('/categories', (req, res) => {
         res.status(404).send("Error!")
     })
 
+})
+
+app.get('/posts/add', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/addPost.html'));
 })
 
 app.use((req, res) => {
