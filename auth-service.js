3f8a9a0b46b259453195bb92a188551ed4d0e10b
coolblast-coder <lyapunovantik@gmail.com>
2022-07-15 23:47:10 -0400

    changed mongodb credentials

--- auth-service.js
+++ auth-service.js
@@ -15,7 +15,7 @@ let User; // to be defined on new connection (see initialize)
 
 module.exports.initialize = function () {
     return new Promise(function (resolve, reject) {
-        let db = mongoose.createConnection("mongodb+srv://alyapunov:v6TbRXiHhHXzUUvu@senecaweb.1wzid.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+        let db = mongoose.createConnection("mongodb+srv://alyapunov:v6TbRXiHhHXzUUvu@senecaweb.1wzid.mongodb.net/?retryWrites=true&w=majority");
 
         db.on('error', (err)=>{
             reject(err); // reject the promise with the provided error
